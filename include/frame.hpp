d04af4a8bd9eaf6faf2c8d82daca5d52ae42e714
andrea-segalini <segalini.ndr@gmail.com>
2014-12-06 15:44:59 +0100

    aggiunto frame.hpp, il commit precendente era errato

---
+++ include/frame.hpp
@@ -0,0 +1,28 @@
+#ifndef FRAME_HPP
+#define FRAME_HPP
+
+#include <iostream> // new
+#include <cstddef>  //size_t
+
+#include <stdint.h> // uint32_t
+#include <stdio.h>  // 
+#include <string.h> // memcpy
+
+class Frame{
+private:
+  uint32_t num;
+  char * data;
+  size_t data_len;
+public:
+  Frame(const char * buffer, const size_t & len);
+  Frame(const uint32_t & n, const char * d, const size_t & len);
+  ~Frame();
+  Frame(const Frame & f);
+
+  uint32_t get_num() const;
+  char * get_data() const;
+
+  size_t serialize(char * byte_array) const;
+};
+
+#endif // FRAME_HPP
