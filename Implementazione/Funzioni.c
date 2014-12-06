805e6f42338af8e1229e31c31d6816dbb8b98ebe
andrea-segalini <segalini.ndr@gmail.com>
2014-12-06 15:25:23 +0100

    eliminata roba

--- Implementazione/Funzioni.c
+++
@@ -1,7 +0,0 @@
-static void transmit_frame(MSG *msg, FRAMEKIND kind,
-static void application_ready(CnetEvent ev, CnetTimerID timer, CnetData data)
-static void physical_ready(CnetEvent ev, CnetTimerID timer, CnetData data)
-static void draw_frame(CnetEvent ev, CnetTimerID timer, CnetData data)
-static void timeouts(CnetEvent ev, CnetTimerID timer, CnetData data)
-static void showstate(CnetEvent ev, CnetTimerID timer, CnetData data)
-void reboot_node(CnetEvent ev, CnetTimerID timer, CnetData data)
