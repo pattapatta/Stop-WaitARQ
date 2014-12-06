b644fbae34209089988c3f905d8b08db6fed9ab1
andrea-segalini <segalini.ndr@gmail.com>
2014-12-06 17:48:26 +0100

    scritto il metodo swap in frame, il ctor senza param di frame, il getter get_data_len() in frame e il receive_nbyte in server_swarq_templates.hpp

--- include/server_swarq.hpp
+++ include/server_swarq.hpp
@@ -1,16 +1,17 @@
 #ifndef SERVERSWARQ_HPP
 #define SERVERSWARQ_HPP
 
-#include <frame.hpp>
+#include <algorithm> // std::copy
+
+#include "frame.hpp"
 
 template<typename OIter>
 class ServerSWARQ{
 public:
   ServerSWARQ(boost::asio::io_service io_service, 
 	      const unsigned short & p);
-  size_t receive_nbyte(OIter out, size_t nbyte);
+  OIter receive_nbyte(OIter out, size_t nbyte);
   Frame receive_frame();
-  void reset_frame_cnt();
 private:
   uint32_t frame_counter;
   boost::asio::io_service & io_service;
--- include/server_swarq_templates.hpp
+++ include/server_swarq_templates.hpp
@@ -2,7 +2,7 @@
 #define SERVERSWARQ_TEMPLATES_HPP
 
 template<typename OIter>
-ServerSWARQ::ServerSWARQ(boost::asio::io_service,
+ServerSWARQ<OIter>::ServerSWARQ(boost::asio::io_service,
 			 const unsigned short & p)
   : port(p){
   
@@ -10,6 +10,42 @@ ServerSWARQ::ServerSWARQ(boost::asio::io_service,
   
   
 }
+
+/* funzione che dato un numero di byte da leggere,
+ * riceve pacchetti dalla rete attraverso il protocollo
+ * stop and wait ARQ. Accoda i pacchetti ricevuti in un
+ * output iterator per comporre l'insieme di dati completo.
+ *
+ * ritorna la posizione dell'ultimo byte scritto nell'output
+ * itearator 
+ */
+template<typename OIter>
+OIter ServerSWARQ<OIter>::ServeSWARQ(OIter out, size_t nbyte){
+  size_t nbyte_recv = 0;
+  
+  while(nbyte_recv < nbyte){
+    Frame frame();
+
+    // riempio il frame con header e payload ricevuti
+    receive_frame(frame);
+    
+    //controllo se il frame ricevuto è doppio
+    if(frame_counter != frame.get_num()){
+      // accetto il frame e lo accodo nell'output
+      char * begin = frame.get_data();
+      char * end = begin + frame.get_data_len();
+      out = std::copy(begin, end, out);
+
+      // aggiorno il frame_counter (modulo UINT32 MAX per
+      // evitare overflow
+      frame_counter = (frame_counter + 1) % UINT_32_MAX;
+
+      // aggiorno il counter dei byte ricevuti con la grandezza
+      // del payload del frame
+      n_byte_recv += frame.get_data_len();
+    }
+  }
+} 
 
 
 
